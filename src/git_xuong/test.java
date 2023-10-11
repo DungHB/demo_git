5fffa327a2af72cb898e7af92252c5552cab53fe
tutbph46367 <tutbph46367@fpt.edu.vn>
2023-10-11 10:30:31 +0700

    tunew

--- src/git_xuong/test.java
+++ src/git_xuong/test.java
@@ -34,9 +34,9 @@ public class test extends javax.swing.JFrame {
         jLabel4 = new javax.swing.JLabel();
         txtTuoi = new javax.swing.JTextField();
         txtDiaChi = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
+        btnDelete = new javax.swing.JButton();
+        btnUpdate = new javax.swing.JButton();
+        btnadd = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -60,11 +60,11 @@ public class test extends javax.swing.JFrame {
 
         txtDiaChi.setFont(new java.awt.Font("Segoe UI", 0, 18)); // NOI18N
 
-        jButton1.setText("jButton1");
+        btnDelete.setText("delete");
 
-        jButton2.setText("jButton2");
+        btnUpdate.setText("update");
 
-        jButton3.setText("jButton3");
+        btnadd.setText("add");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -85,11 +85,11 @@ public class test extends javax.swing.JFrame {
                             .addComponent(txtDiaChi)))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGap(91, 91, 91)
-                        .addComponent(jButton3)
+                        .addComponent(btnadd)
                         .addGap(83, 83, 83)
-                        .addComponent(jButton1)
+                        .addComponent(btnDelete)
                         .addGap(97, 97, 97)
-                        .addComponent(jButton2)))
+                        .addComponent(btnUpdate)))
                 .addGap(154, 154, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -112,9 +112,9 @@ public class test extends javax.swing.JFrame {
                     .addComponent(jLabel4))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 57, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton2)
-                    .addComponent(jButton1)
-                    .addComponent(jButton3))
+                    .addComponent(btnUpdate)
+                    .addComponent(btnDelete)
+                    .addComponent(btnadd))
                 .addGap(19, 19, 19))
         );
 
@@ -130,7 +130,7 @@ public class test extends javax.swing.JFrame {
                     .addGroup(layout.createSequentialGroup()
                         .addGap(127, 127, 127)
                         .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
-                .addContainerGap(193, Short.MAX_VALUE))
+                .addContainerGap(202, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -181,9 +181,9 @@ public class test extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
+    private javax.swing.JButton btnDelete;
+    private javax.swing.JButton btnUpdate;
+    private javax.swing.JButton btnadd;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
